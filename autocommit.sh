b95f0295a008c46ef717d866516988fb00907378
Victor Gan <victorgan@gmail.com>
2015-02-27 10:58:35 -0800

    Changes

--- autocommit.sh
+++ autocommit.sh
@@ -1,3 +1,9 @@
+# edit file
+# $ crontab -e
+# and append
+# 26 6 * * * $HOME/projects/research-log/autocommit.sh
+# to run at 6:26am every day
+
 # Get repository directory
 export REPO_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
